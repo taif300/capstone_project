44502eed09a0729e0af031704654bed17a36c0a8
taif300 <taif.alahmadi300@gmail.com>
2025-03-24 01:26:33 +0300

    Update update_app.sh

--- update_app.sh
+++ update_app.sh
@@ -5,22 +5,29 @@ set -e  # Exit on any error
 date
 echo "Updating Python application on VM..."
 
-REPO_URL="github.com/taif300/capstone_project.git"
+REPO_URL="git@github.com:taif300/capstone_project.git"
 BRANCH="main"
-GITHUB_TOKEN=$TOKEN  # Passed securely via protectedSettings
-HOME_DIR=$(eval echo ~$USER)
+HOME_DIR="$HOME"
 APP_DIR="$HOME_DIR/capstone_project"
+
+# Ensure SSH key exists
+if [ ! -f "$HOME/.ssh/id_rsa" ]; then
+    echo "ERROR: SSH key not found! Ensure you have an SSH key added to GitHub."
+    exit 1
+fi
 
 # Update code
 if [ -d "$APP_DIR" ]; then
-    sudo -u azureuser bash -c "cd $APP_DIR && git fetch origin && git reset --hard origin/$BRANCH"
+    cd "$APP_DIR"
+    git fetch origin
+    git reset --hard origin/$BRANCH
 else
-    sudo -u azureuser git clone -b "$BRANCH" "https://${GITHUB_TOKEN}@${REPO_URL}" "$APP_DIR"
+    git clone -b "$BRANCH" "$REPO_URL" "$APP_DIR"
 fi
 
 # Install dependencies
-sudo -u azureuser $HOME_DIR/miniconda3/envs/project/bin/pip install --upgrade pip
-sudo -u azureuser $HOME_DIR/miniconda3/envs/project/bin/pip install -r "${APP_DIR}/requirements.txt"
+"$HOME_DIR/miniconda3/envs/project/bin/pip" install --upgrade pip
+"$HOME_DIR/miniconda3/envs/project/bin/pip" install -r "${APP_DIR}/requirements.txt"
 
 # Restart services and check status
 sudo systemctl restart backend
