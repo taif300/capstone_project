1655b882f25bf948272155240264b1687b294c0e
taif300 <taif.alahmadi300@gmail.com>
2025-03-24 01:48:43 +0300

    Update update_app.sh

--- update_app.sh
+++ update_app.sh
@@ -5,11 +5,11 @@ set -e  # Exit on any error
 date
 echo "Updating Python application on VM..."
 
-REPO_URL="github.com/stantaov/chatbot-project.git"
+REPO_URL="github.com/taif300/capstone_project.git"
 BRANCH="main"
 GITHUB_TOKEN=$TOKEN  # Passed securely via protectedSettings
 HOME_DIR=$(eval echo ~$USER)
-APP_DIR="$HOME_DIR/chatbot-project"
+APP_DIR="$HOME_DIR/capstone_project"
 
 # Update code
 if [ -d "$APP_DIR" ]; then
