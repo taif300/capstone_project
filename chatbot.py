1368fa31bf6fa13d70980f40719a2aacdd60bde3
Taif Al-Ahmadi <taif.alahmadi300@gmail.com>
2025-03-25 06:45:17 +0300

    title modified to V3

--- chatbot.py
+++ chatbot.py
@@ -154,7 +154,7 @@ with st.sidebar:
         st.button("Delete Chat", on_click=delete_chat)
 
 # Main Content
-st.title("Chatbot Application V2.1")
+st.title("Chatbot Application V3")
 
 if st.session_state["current_chat"]:
     chat_id = st.session_state["current_chat"]
