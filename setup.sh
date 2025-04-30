3e175bd89de11d2fd734ecb55d882891c64f7e07
taif300 <taif.alahmadi300@gmail.com>
2025-04-30 03:58:03 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ $# -ne 4 ]; then
+    echo "Usage: $0 <PAT_token> <repo_url> <branch_name> <key_vault_name>"
+    exit 1
+fi
+
+# Assign arguments to variables
+PAT_TOKEN="$1"
+REPO_URL="$2"
+BRANCH_NAME="$3"
+REPO_NAME=$(basename "$REPO_URL" .git)
+USER=$(whoami)
+HOME_DIR=$(eval echo ~$USER)
+KEY_VAULT_NAME="$4"
+
+# Set up Conda environment
+echo "Setting up conda environment..."
+source "$HOME_DIR/miniconda3/etc/profile.d/conda.sh"
+if ! conda env list | grep -q "^project "; then
+    conda create -y -n project python=3.11
+fi
+
+# Clone the repository
+echo "Cloning repository..."
+cd "$HOME_DIR"
+if [ -d "$REPO_NAME" ]; then
+    echo "Directory $REPO_NAME already exists. Please remove it or choose a different repository."
+    exit 1
+fi
+export GITHUB_TOKEN="$PAT_TOKEN"
+git clone -b "$BRANCH_NAME" "https://${GITHUB_TOKEN}@${REPO_URL}"
+if [ $? -ne 0 ]; then
+    echo "Failed to clone repository"
+    exit 1
+fi
+cd "$REPO_NAME"
+
+# Install requirements
+echo "Installing requirements..."
+if [ -f requirements.txt ]; then
+    "$HOME_DIR/miniconda3/envs/project/bin/pip" install -r requirements.txt
+else
+    echo "No requirements.txt found"
+fi
+
+sudo -u "azureuser tee /home/azureuser/$REPO_NAME/.env" <<EOF
+KEY_VAULT_NAME=$KEY_VAULT_NAME
+EOF
+
+# Create systemd services
+echo "Creating systemd services..."
+cat <<EOF | sudo tee /etc/systemd/system/chromadb.service
+[Unit]
+Description=ChromaDB
+After=network.target
+
+[Service]
+Type=simple
+User=$USER
+WorkingDirectory=$HOME_DIR/$REPO_NAME
+ExecStart=$HOME_DIR/miniconda3/envs/project/bin/chroma run --path $HOME_DIR/$REPO_NAME/chroma_db --host 0.0.0.0
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat <<EOF | sudo tee /etc/systemd/system/frontend.service
+[Unit]
+Description=Streamlit
+After=network.target
+
+[Service]
+Type=simple
+User=$USER
+WorkingDirectory=$HOME_DIR/$REPO_NAME
+ExecStart=$HOME_DIR/miniconda3/envs/project/bin/streamlit run chatbot.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd and start services
+echo "Reloading systemd and starting services..."
+sudo systemctl daemon-reload
+for service in chromadb  frontend; do
+    sudo systemctl enable $service
+    sudo systemctl start $service
+done
+
+echo "Setup completed successfully"
